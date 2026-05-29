1a7d8fe54433e185840ace2ba210a6f78a66f35e
Paulo Amaral <juninhoamaral_2006@hotmail.com>
2026-05-28 23:36:33 -0300

    Ajuste header

---
+++ projeto-final/src/components/Header.jsx
@@ -0,0 +1,47 @@
+import React from "react"
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faEnvelope, faCartShopping, faUser, faBoxOpen, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import "../styles/Header.css"
+
+
+function Header(){
+    return (
+        <header>
+            <nav className="nav">
+                <div className="menu-sup">
+                    <div>
+                        {/* Criar link para home */}
+                        <img src="/img/.png" alt="" />
+                        <FontAwesomeIcon icon={faBoxOpen} />
+                    </div>
+                    <div className="barra-pesquisa">
+                        <input type="text" placeholder="O que você procura?"/>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} className="lupa"/>
+                    </div>
+                    <div>
+                        <ul>
+                            <li><FontAwesomeIcon icon={faCartShopping}/></li>
+                            <li><FontAwesomeIcon icon={faUser} /></li>
+                        </ul>
+                    </div>
+                </div>
+                
+
+                <div className="menu-bot">
+                    <ul>
+                        <li><a href="">Colecionaveis</a></li>
+                        <li><a href="">Chaveiros</a></li>
+                        <li><a href="">Decorações</a></li>
+                        <li><a href="">Organizadores</a></li>
+                        <li><a href="">Jogos</a></li>
+                        <li><a href="">Brinquedos</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    )
+
+}
+
+export default Header
