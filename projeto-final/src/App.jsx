5044c7dde9c3170fef1ca738da3d0b77d36e404c
Paulo Amaral <juninhoamaral_2006@hotmail.com>
2026-06-15 22:17:59 -0300

    ajustes

--- projeto-final/src/App.jsx
+++ projeto-final/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/produto/:id" element={<DetalhesProdutos />} />
       </Routes>
-      <Footer />
+      <Footer />      
     </ChakraProvider>
   );
 }
